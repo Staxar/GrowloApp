8eacb194bb02222d48e8ed511a3c2c7a6e49a933
madomaga <marcin.domagala@capgemini.com>
2023-07-04 12:54:35 +0200

    firebase

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Redirect } from "expo-router";
-import { View, Text } from "react-native";
-
-const StartPage = () => {
-  return <Redirect href={"/home"} />;
-};
-
-export default StartPage;
