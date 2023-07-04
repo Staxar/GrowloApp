b1b36342c6409eadcd2ac468c3ea3bbada42e2cf
madomaga <marcin.domagala@capgemini.com>
2023-07-04 13:14:32 +0200

    firebase

---
+++ firebase-config.js
@@ -0,0 +1,35 @@
+import {
+  Firebase_apiKey,
+  Firebase_authDomain,
+  Firebase_projectId,
+  Firebase_storageBucket,
+  Firebase_messagingSenderId,
+  Firebase_appId,
+  Firebase_measurementId,
+  Firebase_databaseURL,
+} from "@env";
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth/react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: Firebase_apiKey,
+  authDomain: Firebase_authDomain,
+  projectId: Firebase_projectId,
+  storageBucket: Firebase_storageBucket,
+  messagingSenderId: Firebase_messagingSenderId,
+  appId: Firebase_appId,
+  measurementId: Firebase_measurementId,
+  databaseURL: Firebase_databaseURL,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistance(AsyncStorage),
+});
